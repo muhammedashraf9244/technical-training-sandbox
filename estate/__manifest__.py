44db9d18a97dc5ffac868e93bf22973239068a6c
Mohammed Ashraf <hom9244.9244@gmail.com>
2023-08-28 18:10:38 +0300

    [ADD]estate:Initial commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': '',
+    'sequence': 15,
+    'summary': 'The Real Estate Advertisement module',
+    'description': "The Real Estate Advertisement module",
+    'website': '',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
